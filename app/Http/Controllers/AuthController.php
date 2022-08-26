ec1409ce9f9bc1387d7f22a8c41cf974a4736e74
beltranbot <beltranbot@gmail.com>
2022-08-26 02:06:48 +0000

    fixed authservice import

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\DTO\UserRegisterDTO;
 use App\Http\Requests\API\RegisterPostRequest;
 use App\Models\User;
-use App\Service\AuthService;
+use App\Services\AuthService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
